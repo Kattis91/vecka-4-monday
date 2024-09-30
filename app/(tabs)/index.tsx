aeccc67761ca27441e51dac648dd200479bbe909
Kattis91 <katya.svedmark@gmail.com>
2024-09-30 14:21:26 +0200

    start

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,104 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, FlatList, TextInput, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+import { FancyPersonRow } from '../FancyPersonRow';
+
 
 export default function HomeScreen() {
+
+  const [people, setPeople] = useState ([{
+      firstname: "Arne", 
+      lastname: "Arnesson"
+    },{
+      firstname: "Bengt",
+      lastname: "Bengtsson"
+    }, {
+      firstname: "Cecilia",
+      lastname: "Ceciliasson"
+    }, {
+      firstname: "David",
+      lastname: "Davidsson"
+    }
+  ]);
+
+  const [myname, setMyname] = useState("");
+  const [mylastname, setMylastname] = useState("");
+  const [myerrormessage, setMyerrormessage] = useState("");
+
+  function addPerson () {
+    console.log(myname);
+
+    if (myname == "") {
+      // Inget förnamn
+      setMyerrormessage("Inget förnamn");
+      return;
+    }
+
+    if (mylastname == "") {
+      // Inget efternamn
+      setMyerrormessage("Inget efternamn")
+      return;
+    }
+
+    setMyerrormessage("");
+  
+    /*
+    people.push({
+      firstname: "Erik",
+      lastname: "Eriksson"
+    });
+    */
+
+    setPeople([...people, {firstname: myname, lastname: mylastname }]);
+
+    setMyname("");
+    setMylastname("");
+
+    console.log(people);
+
+  }
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ backgroundColor: "blue", flex: 1 }}> 
+
+      <View style={{ backgroundColor: "red", height: 100 }}></View>
+
+      {myerrormessage != "" &&
+      <Text style={nicestyle.nicetext}>{myerrormessage}</Text>
+      }
+
+      <TextInput 
+        onChangeText={setMyname} 
+        value={myname} 
+        style={{ backgroundColor: "white", height: 50 }}/>
+
+      <TextInput 
+        onChangeText={setMylastname} 
+        value={mylastname} 
+        style={{ backgroundColor: "white", height: 50 }}/>
+
+      <Button title='Add' onPress={addPerson} />
+      
+
+      <FlatList 
+        data={people} 
+        renderItem={({item}) =>  <FancyPersonRow firstname={ item.firstname } lastname={ item.lastname }/> }
+      />
+
+    </View>
   );
 }
+
+export const nicestyle = StyleSheet.create({
+  nicetext: {
+    fontSize: 25,
+    color: "black"
+  }
+})
 
 const styles = StyleSheet.create({
   titleContainer: {
